b3804a7b0ab4f08a9736e12f09a9c988461dcbd2
nglinh291 <88537692+nglinh291@users.noreply.github.com>
2021-10-10 00:03:57 +0700

    Add files via upload

--- SS1/sign_up.js
+++ SS1/sign_up.js
@@ -1,63 +1,62 @@
-// function store(){
-
-//     var email = document.getElementById('email');
-//     var name = document.getElementById('name');
-//     var pw = document.getElementById('pw');
-//     const login = document.querySelector("#sign_up");
-
-//     if(name.value.length == 0){
-//         alert('Please fill in username');
-
-//     }else if(pw.value.length == 0){
-//         alert('Please fill in password');
-
-//     }else if(email.value.length == 0){
-//         alert('Please fill in email');
-
-//     }else if(name.value.length == 0 && pw.value.length == 0 && email.value.length == 0){
-//         alert('Please fill in information');
-
-//     }else if(name.value.length > 8){
-//         alert('Username max 8 letters');
-
-//     }else{
-//         localStorage.setItem('email', email.value);
-//         localStorage.setItem('name', name.value);
-//         localStorage.setItem('pw', pw.value);
-//         // alert('Your account has been created');
-//         // login.onclick = () => {
-//         window.location.href = "contact.html"//move to log in page
-        
-        
-//     }
-// }
-
-
-const account_details = [
-    {
-    email: "linh@gmail.com",
-    name: "linh",
-    pw: 123456
-    },
-    {
-    email: "email: ngoc@gmail.com",
-    name: "ngoc",
-    pw: 987643
-    }
-];
-
-
-let account;
-
-if (localStorage.getItem("email") !== null && localStorage.getItem("name") !== null && localStorage.getItem("pw") !== null){
-    account = localStorage.getItem("account_details");
-    // account = localStorage.getItem("name");
-    // account = localStorage.getItem("pw");
-    account = JSON.parse(account)
+let accounts; //Tạo biến để chứa các danh sách accounts
+if(localStorage.getItem("accounts") != null){
+    accounts = JSON.parse(localStorage.getItem("accounts"));
 } else {
-    account = [];
+    accounts = []
 }
-console.log(
-    "Sign up successfully!",
-    account
-);
+
+function store(event){ //lưu thông tin
+    event.preventDefault();
+
+    var email = document.getElementById('email');
+    var name = document.getElementById('name');
+    var pw = document.getElementById('pw');
+    const login = document.querySelector("#sign_up");
+
+    // kiểm tra kiểu dữ liệu có đạt yêu cầu không
+    if(name.value.length == 0){
+        alert('Please fill in username');
+
+    }else if(pw.value.length == 0){
+        alert('Please fill in password');
+
+    }else if(email.value.length == 0){
+        alert('Please fill in email');
+
+    }else if(name.value.length == 0 && pw.value.length == 0 && email.value.length == 0){
+        alert('Please fill in information');
+
+    }else if(name.value.length > 8){
+        alert('Username max 8 letters');
+
+    }else{ //tạo 1 object chứa biến
+        const data = {
+            email: email.value,
+            name: name.value,
+            pw: pw.value
+        };
+
+    // vòng lặp để check
+    let data = JSON.parse(localStorage.getItem("account")) || []
+    let exist = data.length
+    let loginSuccess = false;
+    let index;
+    for (let i = 0; i< accounts.length; i++){
+        if (
+            accounts[i].name == name && accounts[i].pw == pw
+        ) {
+            loginSuccess = true;
+            index = i;
+        }
+    }
+
+    if (!exist && !loginSuccess) {
+        alert("Account exists"); // check tài khoản tồn tại
+    } else {
+        accounts.push(account);
+        localStorage.setItem("accounts", JSON.stringify(accounts));
+        location.href = "contact.html";
+    }
+    e.preventDefault();
+    }}
+
